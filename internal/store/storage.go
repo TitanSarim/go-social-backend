e06554407e7fd68c0d7fc8c7dc24f281c3b6d12c
TitanSarim <muhammadsarim203@gmail.com>
2025-03-24 01:27:14 +0500

    filters completed

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -17,7 +17,7 @@ type Storage struct {
 		GetByID(context.Context, int64) (*Post, error)
 		Delete(context.Context, int64) error
 		Patch(context.Context, int64, *Post) error
-		GetUserFeed(context.Context, int64) ([]PostWithMetadata, error)
+		GetUserFeed(context.Context, int64, PaginatedFeedQuery) ([]PostWithMetadata, error)
 	}
 	Users interface {
 		Create(context.Context, *User) (*User, error)
