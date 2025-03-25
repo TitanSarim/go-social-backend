13dd1d6da9a7f5256afaa1a4c0caabff615d4ade
TitanSarim <muhammadsarim203@gmail.com>
2025-03-25 21:05:55 +0500

    logging

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"time"
 
@@ -11,11 +10,13 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	httpSwagger "github.com/swaggo/http-swagger/v2"
+	"go.uber.org/zap"
 )
 
 type application struct {
 	config config
 	store store.Storage
+	logger  *zap.SugaredLogger
 }
 
 type config struct {
@@ -99,7 +100,7 @@ func (app *application) run(mux *chi.Mux) error {
 		IdleTimeout: time.Minute,
 	}
 
-	log.Printf("server has started at localhost%s", app.config.addr)
+	app.logger.Info("server has started at localhost%s", app.config.addr)
 
 	return srv.ListenAndServe()
 }
